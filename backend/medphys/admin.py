7ceaaaf59a6a9f5e8387dc46e104b1046a20438b
Juan <velazquezrj2012@licifug.ugto.mx>
2022-12-02 19:20:06 -0600

    Add model for appendix B NOM-040

---
+++ backend/medphys/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import radioactiveSourcePackageReception
+
+# class MedPhysAdmin(admin.ModelAdmin):
+#     list_display = ('remarks', 'remarks_description', 'supplier', 'receptionDateTime',
+#     'radionuclide', 'halfLife', 'requestedActivity', 'receivedActivity', 'radiationLevel_1m', 'radiationLevel_0m',
+#     'measurementEquipment', 'brandModel', 'serialNumberEquipment', 'serialNumberDetector',
+#     'detectorCalibrationDate', 'calibrationFactor', 'zoneLocation', 'rubbedSurface', 'smearReading',
+#     'equipmentUsed', 'brandModel_smearReading', 'serialNumberEquipment_smearReading',
+#     'serialNumberDetector_smearReading', 'packageAcceptanceReception', 'reasonsAcceptanceReception',
+#     'nameMedPhys')
+
+# Register your models here.
+#admin.site.register(radioactiveSourcePackageReception, MedPhysAdmin)
---
+++ backend/medphys/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+import string
+import random
+
+# Create your models here.
+# def generate_unique_code():
+#     length = 8
+
+#     while True:
+#         code = ''.join(random.choices(string.ascii_uppercase, k=length))
+#         if radioactiveSourcePackageReception.objects.filter(code=code).count() == 0:
+#             break
+
+#     return code
+
+class radioactiveSourcePackageReception:
+    #code = models.CharField(max_length=10, default=generate_unique_code, unique=True)
+    # Appendix B: NOM-040-NUCL-2016
+    # External inspection of the package
+    remarks = models.CharField(max_length=120)
+    remarks_description = models.TextField()
+    # Package identification
+    supplier = models.CharField(max_length=120)
+    receptionDateTime = models.DateTimeField()
+    radionuclide = models.CharField(max_length=20)
+    halfLife = models.DurationField() # Will need to handle time conversion
+    requestedActivity = models.DecimalField()
+    receivedActivity = models.DecimalField()
+    # Radiological examination of the package
+    radiationLevel_1m = models.DecimalField() # Will need to handle activity units
+    radiationLevel_0m = models.DecimalField()
+    measurementEquipment = models.CharField(max_length=50)
+    brandModel = models.CharField(max_length=120)
+    serialNumberEquipment = models.CharField(max_length=50)
+    serialNumberDetector = models.CharField(max_length=50)
+    detectorCalibrationDate = models.DateTimeField()
+    calibrationFactor = models.CharField(max_length=50)
+    # Surface contamination check (when there is suspicion of contamination)
+    # Would be great if these fields pop up when requested (perhaps with a button)
+    zoneLocation = models.TextField()
+    rubbedSurface = models.TextField()
+    smearReading = models.DecimalField() # Will need to handle activitiy units
+    equipmentUsed = models.CharField(max_length=50)
+    brandModel_smearReading = models.CharField(max_length=120)
+    serialNumberEquipment_smearReading = models.CharField(max_length=50)
+    serialNumberDetector_smearReading = models.CharField(max_length=50)
+    packageAcceptanceReception = models.CharField(max_length=20)
+    reasonsAcceptanceReception = models.TextField()
+    nameMedPhys = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+
+
+
+
+
