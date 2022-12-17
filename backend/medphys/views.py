7e1101d67f42a94a0ff607f040f2ab659b68eac5
Juan <velazquezrj2012@licifug.ugto.mx>
2022-12-16 19:30:49 -0600

    Update views for first API

--- backend/medphys/views.py
+++ backend/medphys/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import radSourceRecSerializer
+from .models import radioactiveSourcePackageReception
 
 # Create your views here.
+
+class radSourceRecView(viewsets.ModelViewSet):
+    serializer_class = radSourceRecSerializer
+    queryset = radioactiveSourcePackageReception.objects.all()
